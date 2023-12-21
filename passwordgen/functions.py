c2855e6b0bb6d5c7a1f48d8945aedebd25ecea74
sustagantos <gustavo.s.goncalves@unesp.br>
2023-12-20 21:01:51 -0300

    some minor changes

---
+++ passwordgen/functions.py
@@ -0,0 +1,38 @@
+import letters as lt
+
+
+
+separator = "_______________________________________________________"
+
+def fpasslen():        #checks if passlen is a valid number
+    print("\n{}".format(separator))
+    print("first, you must type how long your password will be\n")
+    while True:
+        passlen = input("type how long your password will be:       ").strip()
+        if passlen != "0" and passlen.isdigit() == True :
+            break
+        else:
+            print("{} is not valid!\n".format(passlen))
+            continue
+    return int(passlen)   
+
+def table(passlen):        #checks if user wants to use special chars
+    print("\ndone! your password will have {} chars!".format(passlen))
+    print("\n{}".format(separator))
+    print("\nnow, you have to choose if it will use special chars.")
+    while True:
+        b = input("type 1 if you want to use special chars, and 2 if you don't:     ")
+        if b.isdigit()== True:
+            if int(b)==1:
+                print("\nyou want to use special chars!")
+                return True
+            if int(b)==2:
+                print("\nyou don't want to use special chars!")
+                return False
+            else:
+                print("\nyou didn't chose 1 or 2!")
+                continue
+        else:
+            print("\nyou must type separator number!")
+            continue
+
--- passwordgen/main.py
+++ passwordgen/main.py
@@ -1,16 +1,14 @@
 import random
 import letters as lt
+import functions as fun
 
-
-numascii = []
-passw = []      #password
+numascii = []   #creates a list where the numbers in ascii will be
+passw = []      #password will be stored here
 separator = "_______________________________________________________"
 
 
-table1 = [lt.alphl,lt.alphu,lt.num,lt.other]        #list of lists, with other
-table2 = [lt.alphl,lt.alphu,lt.num]     #list of lists, without other
-
-
+table1 = [lt.alphl,lt.alphu,lt.num,lt.other]        #list of lists, with other chars
+table2 = [lt.alphl,lt.alphu,lt.num]     #list of lists, without other chars
 
 for i in range(48,58):      #numbers in the ascii table
     numascii.append(i)
@@ -18,57 +16,26 @@ for i in range(48,58):      #numbers in the ascii table
 
 print("\nPASSWORD GENERATOR!!\n")
 
-def fpasslen():        #checks if passlen is a valid number
-    print("\n{}".format(separator))
-    print("first, you must type how long your password will be\n")
-    while True:
-        passlen = input("type how long your password will be:       ").strip()
-        if passlen != "0" and passlen.isdigit() == True :
-            break
-        else:
-            print("{} is not valid!\n".format(passlen))
-            continue
-    return passlen
 
-passlen = int(fpasslen())       #is the length of the password
+passlen = fun.fpasslen()       #is the length of the password
 
-def table():        #checks if user wants to use special chars
-    print("\ndone! your password will have {} chars!".format(passlen))
-    print("\n{}".format(separator))
-    print("\nnow, you have to choose if it will use special chars.")
-    while True:
-        b = input("type 1 if you want to use special chars, and 2 if you don't:     ")
-        if b.isdigit()== True:
-            if int(b)==1:
-                print("\nyou want to use special chars!")
-                return 1
-            if int(b)==2:
-                print("\nyou don't want to use special chars!")
-                return 0
-            else:
-                print("\nyou didn't chose 1 or 2!")
-                continue
-        else:
-            print("\nyou must type separator number!")
-            continue
+specialchar = fun.table(passlen)       #equals 1 is user wants special chars, and 0 if doesn't
 
-t=table()       #equals 1 is user wants special chars, and 0 if doesn't
+def main(passw):
 
-def main():
-    passw = []
-
-    if t == 0:        #without other
+    if not specialchar:        #without other
         for j in range(1,passlen+1):   
             passw.append(random.choice(random.choice(table2)))
         passw = "".join(passw)
         return passw
         
-    elif t == 1:      #with other
+    elif specialchar:      #with other
         for j in range(1,passlen+1):   
             passw.append(random.choice(random.choice(table1)))
         passw = "".join(passw)
         return passw
 
 print("\n{}".format(separator))
-truepass = main()
-print("your password is:\n{}\n".format(truepass))
+
+passw = main(passw)  
+print("your password is:\n{}\n".format(passw))
